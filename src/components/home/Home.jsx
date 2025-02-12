0cb61b46862ca52401bff367a8ac069c1b2fa36b
s7eamy <ragulis.aurimas@gmail.com>
2025-02-12 22:33:16 +0200

    feat: initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+	return (
+		<div className="container">
+			<h1>Learn To</h1>
+			<p>A platform to reach your studying goals</p>
+			<hr />
+			<button>Create a flashcard</button>
+			<button>View flashcards</button>
+		</div>
+	);
+};
+
+export default Home;
