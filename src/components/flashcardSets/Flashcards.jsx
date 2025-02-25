38defe9851729a4fa5718174287a0ea1db0b76cb
s7eamy <ragulis.aurimas@gmail.com>
2025-02-25 12:08:43 +0200

    feat: implement flashcard set view

---
+++ src/components/flashcardSets/Flashcards.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Container, Typography } from "@mui/material";
+
+const Flashcards = () => {
+	const id = useParams()["setId"];
+	const [set, setSet] = useState(null);
+	console.log(id);
+
+	useEffect(() => {
+		fetch(`/api/flashcards/${id}`)
+			.then((res) => {
+				console.log(res);
+				return res.json();
+			})
+			.then((set) => setSet(set));
+	}, [id]);
+
+	if (!set) return <div>Loading...</div>;
+
+	return (
+		<div>
+			<Container>
+				<Typography variant="h3">Flashcard set {set.title}</Typography>
+			</Container>
+		</div>
+	);
+};
+
+export default Flashcards;
