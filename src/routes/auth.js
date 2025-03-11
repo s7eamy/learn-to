1327b71e240b6d1f01806be7e1e22edbe5045fb4
s7eamy <ragulis.aurimas@gmail.com>
2025-03-11 18:21:00 +0200

    feat: start implementing auth routes

---
+++ src/routes/auth.js
@@ -0,0 +1,63 @@
+import express from "express";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import crypto from "crypto";
+import db from "../db/database.js";
+const router = express.Router();
+
+passport.use(
+	new LocalStrategy(function verify(username, password, cb) {
+		db.get(
+			"SELECT * FROM users WHERE username = ?",
+			[username],
+			function (err, row) {
+				if (err) {
+					return cb(err);
+				}
+				if (!row) {
+					return cb(null, false, {
+						message: "Incorrect username or password.",
+					});
+				}
+
+				crypto.pbkdf2(
+					password,
+					row.salt,
+					310000,
+					32,
+					"sha256",
+					function (err, hashedPassword) {
+						if (err) {
+							return cb(err);
+						}
+						if (
+							!crypto.timingSafeEqual(
+								row.hashed_password,
+								hashedPassword
+							)
+						) {
+							return cb(null, false, {
+								message: "Incorrect username or password.",
+							});
+						}
+						return cb(null, row);
+					}
+				);
+			}
+		);
+	})
+);
+
+router.get("/", (req, res) => {
+	res.json({ msg: "Hello from the auth route!" });
+});
+
+router.post(
+	"/login/password",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/login",
+	})
+);
+
+export default router;
