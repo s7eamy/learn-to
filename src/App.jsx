962192d720ef8f4bdae4a4e9a4a86465ec1f8d95
s7eamy <ragulis.aurimas@gmail.com>
2025-03-11 16:57:34 +0200

    feat: add login frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import FlashcardSets from "./components/flashcardSets/FlashcardSets";
 import Flashcards from "./components/flashcardSets/Flashcards";
 import Dashboard from "./components/Dashboard";
 import Quizzes from "./components/Quizzes";
+import Login from "./components/auth/Login";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Dashboard />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/sets" element={<FlashcardSets />} />
 				<Route path="/sets/:setId" element={<Flashcards />} />
 				<Route path="/quizzes" element={<Quizzes />} />
