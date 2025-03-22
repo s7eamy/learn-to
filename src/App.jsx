fcfc7a7c8319766be875f22c90b99928159f4309
Augustas <augustas.bieliauskas12@gmail.com>
2025-03-22 21:36:20 +0200

    Added flashcard set viewer functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FlashcardSets from "./components/flashcardSets/FlashcardSets";
-import Flashcards from "./components/flashcardSets/Flashcards";
+import FlashcardSets from "./components/flashcardSets/FlashcardSets"; // Updated component
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Quizzes from "./components/Quizzes";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register.jsx";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Dashboard />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/sets" element={<FlashcardSets />} />
-				<Route path="/sets/:setId" element={<Flashcards />} />
-				<Route path="/quizzes" element={<Quizzes />} />
-			</Routes>
-		</Router>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/sets" element={<FlashcardSets />} /> {/* Combined list and viewer */}
+                <Route path="/quizzes" element={<Quizzes />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
