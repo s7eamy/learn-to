46babd26788b0af677e49457bd4e12852d4a7a54
mycius <henrikas.mic@gmail.com>
2025-03-24 19:45:08 +0200

    feat:added backend to handle quiz questions, to make sure that they are saved in the database

--- src/db/database.js
+++ src/db/database.js
@@ -3,8 +3,8 @@ import sqlite3 from "sqlite3";
 const db = process.env.NODE_ENV === 'test' ? new sqlite3.Database(":memory:") : new sqlite3.Database("./app.db");
 
 db.serialize(() => {
-	// Flashcard Sets table
-	db.run(`
+    // Flashcard Sets table
+    db.run(`
     CREATE TABLE IF NOT EXISTS flashcard_sets (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       title TEXT NOT NULL,
@@ -12,8 +12,8 @@ db.serialize(() => {
     )
   `);
 
-	// Flashcards table
-	db.run(`
+    // Flashcards table
+    db.run(`
     CREATE TABLE IF NOT EXISTS flashcards (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       set_id INTEGER,
@@ -23,20 +23,33 @@ db.serialize(() => {
     )
   `);
 
-	// Quizzes table
-	db.run(`
+    // Quizzes table
+    db.run(`
     CREATE TABLE IF NOT EXISTS quizzes (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL
     )
   `);
 
-	db.run(`
+    // Questions table
+    db.run(`
+    CREATE TABLE IF NOT EXISTS questions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      quiz_id INTEGER NOT NULL,
+      text TEXT NOT NULL,
+      answer TEXT NOT NULL,
+      FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+    )
+  `);
+
+    // Users table
+    db.run(`
     CREATE TABLE IF NOT EXISTS users (
       username TEXT NOT NULL PRIMARY KEY,
       salt TEXT NOT NULL,
       hashed_password TEXT NOT NULL
-    )`);
+    )
+  `);
 });
 
 export default db;
