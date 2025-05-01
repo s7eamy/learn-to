636102ff1cb79741c22b84ecc18e2742f5b338a5
xmivte <viktorastimtim@gmail.com>
2025-05-01 14:24:02 +0300

    feat: implemented basic common dashboard that deals with quizzes and flashcards.

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,37 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FlashcardSets from "./components/flashcardSets/FlashcardSets"; // Updated component
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import Quizzes from "./components/Quiz/Quizzes";
-import QuizQuestions from "./components/Quiz/QuizQuestions";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register.jsx";
 import QuizViewer from "./components/Quiz/QuizViewer";
 import FlashcardViewer from "./components/flashcardSets/FlashcardViewer";
-import Flashcards from "./components/flashcardSets/Flashcards.jsx";
-
+import Set_Dashboard from "./components/common/Set_Dashboard.jsx";
+import Quizzes from "./components/Quiz/Quizzes";
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/sets" element={<FlashcardSets />} />{" "}
-        {/* Combined list and viewer */}
-        <Route path="/sets/:setId" element={<Flashcards />} />{" "}
-        {/* Flashcard viewer */}
-        <Route path="/sets/:setId/view" element={<FlashcardViewer />} />{" "}
-        {/* Study cards viewer */}
-        <Route path="/quizzes" element={<Quizzes />} />
-        <Route path="/quizzes/:quizId/questions" element={<QuizQuestions />} />
-        <Route path="/quizzes/:quizId/view" element={<QuizViewer />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/sets" element={<FlashcardSets />} />{" "}
+				{/* Flashcard routes */}
+				<Route path="/sets/:setId" element={<Set_Dashboard />} />{" "}
+				{/* Flashcard viewer */}
+				<Route
+					path="/sets/:setId/view"
+					element={<FlashcardViewer />}
+				/>{" "}
+				{/* Study cards viewer */}
+				<Route path="/quizzes" element={<Quizzes />} />
+				<Route
+					path="/quizzes/:quizId/questions"
+					element={<Set_Dashboard />}
+				/>
+				<Route path="/quizzes/:quizId/view" element={<QuizViewer />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
